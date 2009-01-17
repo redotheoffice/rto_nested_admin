3d81c76406f2d36e33f713c4239bdb967487bbc3
Sjoerd de Jong <rto@weett.nl>
2009-01-17 09:43:50 +0100

    added an instruction for visitors

---
+++ apps/backend/modules/tree/templates/_list_footer.php
@@ -0,0 +1,3 @@
+<i>After changing the order of the tree, the new order should be saved. This is currently implemented
+  as a batch action, so please choose <b>Update tree order</b> from the 'Choose an action' dropdown
+  and click on 'Go' to save the new order.</i>
