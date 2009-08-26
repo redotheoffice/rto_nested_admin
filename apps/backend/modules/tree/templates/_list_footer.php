ac12cf32f3e3bd0a0b23d095c458f9b6e5d91818
Sjoerd de Jong <sjoerd@weett.nl>
2009-08-26 18:16:28 +0200

    minor changes

--- apps/backend/modules/tree/templates/_list_footer.php
+++ apps/backend/modules/tree/templates/_list_footer.php
@@ -1,3 +1,6 @@
+<h3>Important Information:</h3>
 <i>After changing the order of the tree, the new order should be saved. This is currently implemented
   as a batch action, so please choose <b>Update tree order</b> from the 'Choose an action' dropdown
-  and click on 'Go' to save the new order.</i>
+  and click on 'Go' to save the new order.</i><br/>
+  
+<i>Please note that the DB is reset every hour</i>
